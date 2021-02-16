d24b551fc7d7190f61302c6da032d8b77863a5e5
Oliver Paschke <oliver.paschke@bytro.com>
2021-02-16 14:28:42 +0100

    Update and fix layout-bmfont-text and quad-indices

--- quad-indices/index.d.ts
+++ quad-indices/index.d.ts
@@ -1,9 +1,29 @@
 declare module 'quad-indices' {
+    type NumericArray =
+        Float32Array |
+        Float64Array |
+        Uint8ClampedArray |
+        Uint8Array |
+        Uint16Array |
+        Uint32Array |
+        Int8Array |
+        Int16Array |
+        Int32Array |
+        Array<number>;
+
     interface QuadOptions {
-        clockwise: boolean;
-        type: string;
-        count: number;
+        /** default: false */
+        clockwise?: boolean;
+        /** default: 'uint16' */
+        type?: string;
+        /** default: 1 */
+        count?: number;
+        /** default: 0 */
+        start?: number;
     }
-    function createQuadElements(opt: QuadOptions): number[];
-    export = createQuadElements;
+
+    function createQuadElements(opt?: QuadOptions): NumericArray;
+    function createQuadElements(array: NumericArray, opt?: QuadOptions): NumericArray;
+
+    export default createQuadElements;
 }
